583bd9d219e989adf9b50e5a6af42e476d8f3b8c
Joe Perry <japerry16633@gmail.com>
2021-10-10 16:57:17 -0500

    looping hw

--- README.md
+++ README.md
@@ -27,10 +27,18 @@ Create a function that uses a for...of loop and an if statement to console.log t
 Use .map() to map over the arrayOfPersons and console.log() their information.
 
 Use .filter() to filter the persons array and console.log 0only males in the array.
+
 Create a function that returns true if the value of birthDate is before Jan 1, 1990.
+
 Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+
+
+
 BONUS - Create a function that returns true if the date passed to it is >= 21 years in the past.
 BONUS - .filter() out the people in the array who are younger than 21.
+
+
+
 Pt. 2 - From Scratch .find() & .findIndex()
 Whiteboard
 Code plan
--- main.js
+++ main.js
@@ -41,9 +41,9 @@ console.log(Object.keys(personKeys));
 
 // Create a function that logs out the keys and values of the object using Object.entries().
 
-for (const [key, value] of Object.entries(person)) {
-  console.log(`${key}: ${value}`);
-}
+// for (const [key, value] of Object.entries(person)) {
+//   console.log(`${key}: ${value}`);
+// }
 
 
 // Create an arrayOfPersons that contains multiple "people" objects. You can simply copy/paste the person object you made above multiple times. 
@@ -55,21 +55,21 @@ arrayOfPersons = [
     firstName: "Jane",
     lastName: "Doe",
     birthDate: "Jan 5, 1928",
-    gender: "female"
+    gender: "Female"
   } ,
 
   {
     firstName: "Joe",
     lastName: "Fox",
     birthDate: "Apr 26, 1981",
-    gender: "male"
+    gender: "Male"
     } ,
 
     {
         firstName: "Jim",
         lastName: "Bean",
         birthDate: "Aug 15, 1966",
-        gender: "male"
+        gender: "Male"
       } ,
 
     ]
@@ -78,11 +78,52 @@ arrayOfPersons = [
     
 
 
-  for (const element of arrayOfPersons) {
+  
+
+
+  for (const person of arrayOfPersons) {
+const birthDateLength =(person.birthDate.length)
+const lastDigit = (person.birthDate.charAt(birthDateLength -1))
+if (parseInt(lastDigit) % 2 === 0){
+  console.log('the number is even');
+} else {
+  console.log('The number is odd', person.birthDate);
+}
     console.log('valueS:', arrayOfPersons); 
   }
   
 
+  // Use .map() to map over the arrayOfPersons and console.log() their information.
 
-  
+  const map = arrayOfPersons.map(getInfo)
+
+  function getInfo(item) {
+    console.log('person info', [item.firstName, item.lastName, item.birthDate,]);
+  }
+
+
+  // Use .filter() to filter the persons array and console.log 0only males in the array.
+
+
+ arrayOfPersons.filter(checkMale)
+ function checkMale (person) {
+console.log(person.gender);
+if(person.gender === 'Male'){
+  console.log(person.gender === 'Male');
+  console.log(person);
+  return person
+}
+ } 
  
+
+//  Create a function that returns true if the value of birthDate is before Jan 1, 1990.
+
+for (const person of arrayOfPersons) {
+  const birthYear = person.birthDate.substring(person.birthDate.length -4)
+  console.log(birthYear);
+  if (parseInt(birthYear) < 1990) {
+    return true
+  }
+}
+
+// Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
